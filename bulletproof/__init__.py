dcbdf54017c3cbbcd7562a543659865686931567
Beau Bremer <6852572+KnowOneActual@users.noreply.github.com>
2025-12-24 15:35:12 -0600

    Add bulletproof package init

---
+++ bulletproof/__init__.py
@@ -0,0 +1,9 @@
+"""Bulletproof Video Playback - Professional transcoding for theater & streaming."""
+
+__version__ = "0.1.0"
+__author__ = "Beau Bremer"
+__license__ = "MIT"
+
+from bulletproof.core import TranscodeJob, TranscodeProfile  # noqa: F401
+
+__all__ = ["TranscodeJob", "TranscodeProfile"]
