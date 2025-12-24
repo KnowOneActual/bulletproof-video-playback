9cd189fe2cc6bb93e9896f9826d592e192632be0
Beau Bremer <6852572+KnowOneActual@users.noreply.github.com>
2025-12-24 15:35:15 -0600

    Add core module init

---
+++ bulletproof/core/__init__.py
@@ -0,0 +1,6 @@
+"""Core transcoding and codec logic."""
+
+from bulletproof.core.job import TranscodeJob
+from bulletproof.core.profile import TranscodeProfile
+
+__all__ = ["TranscodeJob", "TranscodeProfile"]
