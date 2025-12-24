b07c051cbe38068c6ecc20227f461e2ba44c8582
Beau Bremer <6852572+KnowOneActual@users.noreply.github.com>
2025-12-24 15:39:19 -0600

    Add main package exports

--- bulletproof/__init__.py
+++ bulletproof/__init__.py
@@ -1,9 +1,17 @@
-"""Bulletproof Video Playback - Professional transcoding for theater & streaming."""
+"""bulletproof-video-playback: Professional video transcoding for theater & streaming."""
 
 __version__ = "0.1.0"
 __author__ = "Beau Bremer"
-__license__ = "MIT"
+__email__ = "dev@knowoneactual.com"
 
-from bulletproof.core import TranscodeJob, TranscodeProfile  # noqa: F401
+from bulletproof.core import TranscodeProfile, TranscodeJob, get_profile, list_profiles
+from bulletproof.cli import cli
 
-__all__ = ["TranscodeJob", "TranscodeProfile"]
+__all__ = [
+    "__version__",
+    "TranscodeProfile",
+    "TranscodeJob",
+    "get_profile",
+    "list_profiles",
+    "cli",
+]
